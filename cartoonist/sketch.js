354d6605c549315fa2808415247c6df4bcaa9442
Tory Peterschild <torypeterschild@Torys-MacBook-Pro.local>
2016-06-01 11:53:00 -0400

    adding definitions for body parts

--- cartoonist/sketch.js
+++ cartoonist/sketch.js
@@ -14,15 +14,30 @@ function setup() {
   noFill();
 }
 
+/* Define parts of figure */
+var head = {
+  x: 300,
+  y: 100,
+  radiusX: 60,
+  radiusY: 70,
+};
+
+/* Main draw function */
 function draw() {
   background(0);
   for (var i = 0; i < 200; i += 20) {
     bezier(mouseX-(i/2.0), 40+i, 410, 20, 440, 300, 240-(i/16.0), 300+(i/8.0));
   }
 
+  ellipse(head.x, head.y, head.radiusX, head.radiusY);
+
+  // head.draw();
+
+
   s = "Imagine you're reading a really really cool and funny caption.";
   fill(255);
   textFont(trashHand);
   textSize(30);
+  textLeading(25);
   text(s, 20, 10, 200, 300); // Text wraps within text box
 }
