af5d5266ef1d03517ff8f9c24f97be908a0ff6c8
Tory Peterschild <torypeterschild@Torys-MacBook-Pro.local>
2016-06-07 13:34:47 -0400

    working svg animation of eye

--- cartoonist/sketch_svg.js
+++ cartoonist/sketch_svg.js
@@ -1,8 +1,53 @@
-var s = Snap(800,800);
+var s = new Snap(800,800);
+
+var dog = Snap.select('#dog'),
+  dogStartMatrix = new Snap.Matrix(),
+  dogMidMatrix = new Snap.Matrix();
+console.log(dog);
+var nose = dog.select('#nose');
+var ear = dog.select('#ear');
+var eye = dog.select('#eye');
+var frontleg = dog.select('#frontleg');
+var face = dog.select('#face');
 s.attr({ viewBox: "0 0 600 600" });
 
-var dog = Snap.load("svg/dog.svg", function(f) {
-  var g = f.select("g");
-  s.append(g);
-  g.drag();
+eyeAnimation();
+
+function eyeAnimation(){
+  eye.stop().animate(
+    { transform: 'r90,256,256'},
+    1000,
+    function(){
+      eye.attr({ transform: 'rotate(256,256,256'});
+      eyeAnimation();
+    });
+}
+
+
+// var dog = s.select('.dog'),
+//   dogStartMatrix = new Snap.Matrix(),
+//   dogMidMatrix = new Snap.Matrix();
+
+// dogStartMatrix.rotate(10);
+// dogStartMatrix.translate(0,-50);
+// dogMidMatrix.rotate(-15);
+// dogMidMatrix.translate(300,-20);
+
+dog.animate({
+  transform: dogStartMatrix
+}, 1250, mina.easeinout, function() {
+  dog.animate({
+    transform: dogMidMatrix
+  }, 250);
 });
+
+// Snap.load("svg/dog_01.svg", function(response) {
+//   var dog = response;
+//   s.append(dog);
+//   // s.append(g);
+//   // g.drag();
+//   s.drag();
+// });
+
+var hoverover = function() { dog.animate({ transform: 's2r45,150,150' }, 1000, mina.bounce ) };
+var hoverout = function() { dog.animate({ transform: 's1r0,150,150' }, 1000, mina.bounce ) };
