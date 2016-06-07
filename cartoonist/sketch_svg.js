33f8d0e182385aa8723a4ea046b7a1f6bdc29fd2
Tory Peterschild <torypeterschild@Torys-MacBook-Pro.local>
2016-06-07 17:17:12 -0400

    added text container

--- cartoonist/sketch_svg.js
+++ cartoonist/sketch_svg.js
@@ -192,48 +192,14 @@ function faceAnimation(){
   );
 }
 
-// function raysAnimation(){
-//     sunRays.stop().animate(
-//       { transform: 'r90,256,256'}, // Basic rotation around a point. No frills.
-//       10000, // Nice slow turning rays
-//       function(){ 
-//         sunRays.attr({ transform: 'rotate(0 256 256)'}); // Reset the position of the rays.
-//         raysAnimation(); // Repeat this animation so it appears infinite.
-//       }
-//     );
-  
-//   }
+var t = Snap("#text-container");
 
-// function frontlegAnimation(){
-//   frontleg.stop()
-// }
-
-
-
-// var dog = s.select('.dog'),
-//   dogStartMatrix = new Snap.Matrix(),
-//   dogMidMatrix = new Snap.Matrix();
-
-// dogStartMatrix.rotate(10);
-// dogStartMatrix.translate(0,-50);
-// dogMidMatrix.rotate(-15);
-// dogMidMatrix.translate(300,-20);
-
-dog.animate({
-  transform: dogStartMatrix
-}, 1250, mina.easeinout, function() {
-  dog.animate({
-    transform: dogMidMatrix
-  }, 250);
+Snap.load("svg/svg_alphabet/a.min.svg", function(response) {
+  var a = response;
+  t.append(a);
 });
 
-// Snap.load("svg/dog_01.svg", function(response) {
-//   var dog = response;
-//   s.append(dog);
-//   // s.append(g);
-//   // g.drag();
-//   s.drag();
-// });
-
-var hoverover = function() { dog.animate({ transform: 's2r45,150,150' }, 1000, mina.bounce ) };
-var hoverout = function() { dog.animate({ transform: 's1r0,150,150' }, 1000, mina.bounce ) };
+Snap.load("svg/svg_alphabet/b.min.svg", function(response) {
+  var b = response;
+  t.append(b);
+});
