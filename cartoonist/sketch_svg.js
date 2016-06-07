63491d7b99d911f5487e097cc40c75e9f302a1b1
Tory Peterschild <torypeterschild@Torys-MacBook-Pro.local>
2016-06-07 15:07:12 -0400

    added animations

--- cartoonist/sketch_svg.js
+++ cartoonist/sketch_svg.js
@@ -9,18 +9,99 @@ var ear = dog.select('#ear');
 var eye = dog.select('#eye');
 var frontleg = dog.select('#frontleg');
 var face = dog.select('#face');
+var earandtail = dog.select('#earandtail');
+var faceblob = dog.select('#faceblob');
 // s.attr({ viewBox: "0 0 600 600" });
 
+// var wholeEar = s.group(ear,earandtail);
+// console.log(wholeEar);
+
 eyeAnimation();
+earAnimation();
+innerEarAnimation();
+faceAnimation();
 
 function eyeAnimation(){
   eye.stop().animate(
-    { transform: 'r360,210,80'},
+    { transform: 'r45,210,80'},
     1000,
-    mina.bounce, function() {
+    mina.bounce,
+    function(){
+      eye.attr({ transform: 'rotate(0 256 256'});
       eyeAnimation();
-    } );
+    }
+  );
 }
+
+function earAnimation(){
+  earandtail.stop().animate(
+    { transform: 't0,3'},
+    1000,
+    mina.bounce,
+    function(){
+      earandtail.attr({ transform: 'rotate(0 256 256'});
+      earAnimation();
+    }
+  );
+}
+
+function innerEarAnimation(){
+  ear.stop().animate(
+    { transform: 't0,3'},
+    1000,
+    mina.bounce,
+    function(){
+      ear.attr({ transform: 'rotate(0 256 256'});
+      innerEarAnimation();
+    }
+  );
+}
+
+function faceAnimation(){
+  face.stop().animate(
+    { transform: 't0,3'},
+    1000,
+    mina.bounce,
+    function(){
+      face.attr({ transform: 'rotate(0 256 256'});
+      faceAnimation();
+    }
+  );
+  faceblob.stop().animate(
+    { transform: 't0,3'},
+    1000,
+    mina.bounce,
+    function(){
+      faceblob.attr({ transform: 'rotate(0 256 256'});
+      faceAnimation();
+    }
+  );
+  nose.stop().animate(
+    { transform: 't0,3'},
+    1000,
+    mina.bounce,
+    function(){
+      nose.attr({ transform: 'rotate(0 256 256'});
+      faceAnimation();
+    }
+  );
+}
+
+// function raysAnimation(){
+//     sunRays.stop().animate(
+//       { transform: 'r90,256,256'}, // Basic rotation around a point. No frills.
+//       10000, // Nice slow turning rays
+//       function(){ 
+//         sunRays.attr({ transform: 'rotate(0 256 256)'}); // Reset the position of the rays.
+//         raysAnimation(); // Repeat this animation so it appears infinite.
+//       }
+//     );
+  
+//   }
+
+// function frontlegAnimation(){
+//   frontleg.stop()
+// }
 
 
 
