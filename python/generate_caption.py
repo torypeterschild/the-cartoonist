424fb65b697c1ff420af201e923b01bab5eaff66
Tory Peterschild <torypeterschild@Torys-MacBook-Pro.local>
2016-06-13 16:20:06 -0400

    updated caption generation

--- python/generate_caption.py
+++ python/generate_caption.py
@@ -1,13 +1,20 @@
 from __future__ import print_function
 from nltk.corpus import PlaintextCorpusReader
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
 import nltk
+import nltk.data
 import random
 
+# TODO: deal with names
+# deal with grammar
+# expand corpus
 
 corpus_root = 'corpus/'
 wordlists = PlaintextCorpusReader(corpus_root, '.*')
+sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
 
-def generate_model(cfdist, word, num=45):
+def generate_model(cfdist, word, num=25):
   for i in range(num):
     print(word, end=" ")
     # word = cfdist[word].max()
@@ -15,8 +22,16 @@ def generate_model(cfdist, word, num=45):
     # print("TEMP %s" % temp)
 
 text = wordlists.words()
-bigrams = nltk.bigrams(text)
+text_as_string = " ".join(text)
+
+print('\n-----\n'.join(sent_detector.tokenize(text_as_string.strip())))
+
+s_tokenized = sent_detector.tokenize(text_as_string.strip())
+words_tokenized = word_tokenize(" ".join(s_tokenized))
+words = [w.lower() for w in text if w.isalpha()]
+words1 = sent_tokenize(text_as_string)
+bigrams = nltk.bigrams(words_tokenized)
 cfd = nltk.ConditionalFreqDist(bigrams)
 
-generate_model(cfd, 'mean')
+generate_model(cfd, 'I')
 
