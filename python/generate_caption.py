035cff4e9cb9fd8e0194cc885dc9890c65294c47
Tory Peterschild <torypeterschild@Torys-MacBook-Pro.local>
2016-06-14 11:02:53 -0400

    added caption generator object

--- python/generate_caption.py
+++ python/generate_caption.py
@@ -10,32 +10,115 @@ import random
 # deal with grammar
 # expand corpus
 
-corpus_root = 'corpus/'
-wordlists = PlaintextCorpusReader(corpus_root, '.*')
-sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+""" Define non-alphanumeric characters """
+SENTENCE_TERMINATORS = ['!', '?', '.']
+CLAUSE_TERMINATORS = [',', ';']
+VALID_SYMBOLS = SENTENCE_TERMINATORS + CLAUSE_TERMINATORS
 
-def generate_model(cfdist, word, num=200):
-  for i in range(num):
-    print(word, end=" ")
-    word = random.choice(list(cfdist[word]))
-    # print("TEMP %s" % temp)
-  print("\n\n")
+
+""" Caption Generator Object """
+class CaptionGenerator():
+  def __init__(self, *args):
+    corpus_root = 'corpus/'
+    wordlists = PlaintextCorpusReader(corpus_root, '.*')
+    text = " ".join(wordlists.words())
+    words = nltk.word_tokenize(text)
+    sents = nltk.sent_tokenize(text)
+    bigrams = nltk.bigrams([w.lower() for w in words])
+    self.cdf = nltk.ConditionalFreqDist(bigrams)
+    self.case_cdf = nltk.ConditionalFreqDist([(w.lower(), w) for w in words])
+    self.avg_sent_len = int(len(words)/len(sents))
+
+  def __call__(self, word):
+    word = word.lower()
+    if " " in word:
+      print("space in word")
+      ret = nltk.word_tokenize(word)
+      word = ret[-1]
+    else:
+      print("space not in word")
+      ret = [word]
+
+    while word not in SENTENCE_TERMINATORS:
+      if ret[-1] in CLAUSE_TERMINATORS:
+        print("in clause terminators")
+        prev_word = ret[-1]
+      else:
+        print("not in clause terminators")
+        prev_word = ret[-1]
+      for new_word in self.cdf[word]:
+        # Avoid duplicates
+        if new_word == word:
+          continue
+        # Avoid symbols  
+        if not (new_word.isalpha() or new_word.isdigit()) and not new_word in VALID_SYMBOLS:
+          continue
+        if new_word in SENTENCE_TERMINATORS and len(ret) < self.avg_sent_len/2:
+          continue
+        if not new_word in ret[-len(ret)/2:]:
+          prev_phrase = [prev_word, new_word]
+          if not " ".join(prev_phrase) in " ".join(ret):
+            if not (len(new_word) < 4 and (len(prev_word) < 4) and not random.randint(0,6)):
+              word = new_word
+              if not random.randint(0,2):
+                break
+
+      if word == ret[-1]:
+        ret.append('...')
+        break
+      ret.append(word)  
+
+    for x, w in enumerate(ret[:]):
+      d = self.case_cdf[w]
+      if d:
+        ret[x] = d.max()
+
+    ret = " ".join(ret)
     
-text = wordlists.words()
-text_as_string = " ".join(text)
+    ret = ret[0].upper() + ret[1:]
 
-print('\n-----\n'.join(sent_detector.tokenize(text_as_string.strip())))
+    for symbol in VALID_SYMBOLS:
+      ret = ret.replace(" %s" % symbol, symbol)
+    return ret                     
 
-s_tokenized = sent_detector.tokenize(text_as_string.strip())
-words_tokenized = word_tokenize(" ".join(s_tokenized))
-words = [w.lower() for w in text if w.isalpha()]
-words1 = sent_tokenize(text_as_string)
-bigrams = nltk.bigrams(words_tokenized)
-cfd = nltk.ConditionalFreqDist(bigrams)
+    
+def main():
+  g = CaptionGenerator()
+  print(g)
+  print(g.avg_sent_len)
+  caption = g.__call__("i")
+  print(caption)
 
-init_word = random.choice(list(words_tokenized))
-print(init_word)
-print("\n========\n")
 
-generate_model(cfd, init_word)
+if __name__ == "__main__":
+  main()
+
+# corpus_root = 'corpus/'
+# wordlists = PlaintextCorpusReader(corpus_root, '.*')
+# sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+
+# def generate_model(cfdist, word, num=200):
+#   for i in range(num):
+#     print(word, end=" ")
+#     word = random.choice(list(cfdist[word]))
+#     # print("TEMP %s" % temp)
+#   print("\n\n")
+    
+# text = wordlists.words()
+# text_as_string = " ".join(text)
+
+# print('\n-----\n'.join(sent_detector.tokenize(text_as_string.strip())))
+
+# s_tokenized = sent_detector.tokenize(text_as_string.strip())
+# words_tokenized = word_tokenize(" ".join(s_tokenized))
+# words = [w.lower() for w in text if w.isalpha()]
+# words1 = sent_tokenize(text_as_string)
+# bigrams = nltk.bigrams(words_tokenized)
+# cfd = nltk.ConditionalFreqDist(bigrams)
+
+# init_word = random.choice(list(words_tokenized))
+# print(init_word)
+# print("\n========\n")
+
+# generate_model(cfd, init_word)
 
