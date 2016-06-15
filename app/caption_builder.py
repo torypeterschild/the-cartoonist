9597ccbe35e7a6583c90ca0d5d78793d86532235
Tory Peterschild <torypeterschild@Torys-MacBook-Pro.local>
2016-06-15 15:50:59 -0400

    changed file structure, have working flask app

---
+++ app/caption_builder.py
@@ -0,0 +1,38 @@
+from textblob import TextBlob
+import random
+import sys, os
+from flask import Flask, request, url_for, render_template
+app = Flask(__name__)
+
+
+# @app.route("/")
+def read_file():
+  infile = open("corpus/corpus000.txt", "r")
+  content = infile.read()
+  blob = TextBlob(content.decode('utf-8'))
+  print(type(blob))
+  # ngrams = blob.ngrams(n=3)
+ 
+  sentence_list = list()
+  for sentence in blob.sentences:
+    if len(sentence.words) >= 6:
+      sentence_list.append(sentence.replace("\n", " "))  
+
+  for item in random.sample(sentence_list, 1):
+    item = item.split()
+    sen = []
+    for w in item:
+      sen.append(w)
+    return " ".join(sen)
+
+
+# @app.route("/caption", methods=['GET', 'POST'])
+@app.route("/")
+def main():
+  return render_template('index.html')     
+   
+
+
+if __name__ == "__main__":
+  f = "corpus/corpus000.txt"
+  app.run()
