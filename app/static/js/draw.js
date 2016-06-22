94f80df81163f06906c0c8f15ff306488c023b1a
Tory Peterschild <torypeterschild@Torys-MacBook-Pro.local>
2016-06-22 19:15:34 -0400

    splitting up caption again

--- app/static/js/draw.js
+++ app/static/js/draw.js
@@ -28,16 +28,25 @@ console.log(dog_bb);
 var captionText = $("#caption").text();
 console.log(captionText);
 
-var text = s.text(dog_bb.x, dog_bb.y + dog_bb.height + 30, captionText);
 
 wordCount = captionText.split(" ").length;
 console.log("word count is");
 console.log(wordCount);
 
+
+// var headline = snap.paper.text(56,100, 
+//   ['The Three Layers','of','Every Web Page']).attr({
+//     fill: '#FBAF3F', fontFamily: 'Impact'});
+
+//     headline.select('tspan:first-of-type').attr({fontSize: '2.8em'})
+//     headline.select('tspan:nth-of-type(2)').attr({fill: 'none', stroke: '#FBAF3F', fontSize: '2.2em', dx: '15px', dy: '5px'});
+//     headline.select('tspan:last-of-type').attr({fontSize: '3.6em', x: '56px', y: '160px'});
+
 var phrases = [];
 
-if (wordCount > 5) {
+if (wordCount > 6) {
   console.log("Needs to be split up.");
+  console.log("wordCount/6 = " + Math.round(wordCount/6));
   words = captionText.split(" ");
   phrases.push(words.slice(0,5).join(" "));
   phrases.push(words.slice(5, wordCount).join(" "));
@@ -46,11 +55,38 @@ if (wordCount > 5) {
 
 console.log(phrases);
 
+// var block = s.rect(dog_bb.x - 50, dog_bb.y + dog_bb.height + 30, 500, 200);
+
+captionHeight = dog_bb.y + dog_bb.height - 20;
+
+// block.attr({
+//   fill: "rgb(236, 240, 241)",
+//   stroke: "#1f2c39",
+//   strokeWidth: 3
+// });
+
+var text1 = s.text(dog_bb.x, captionHeight, phrases);
+
 var t = new Snap.Matrix() 
 t.translate(0, 35); 
 t.rotate(5, 0, 40); 
-text.attr({"font-size":40});
-text.transform(t);
+text1.attr({"font-size":40});
+text1.transform(t);
+
+// TODO: make different lines slant differently
+text1.selectAll("tspan").forEach(function(tspan, i){
+      tspan.attr({x:0,y:captionHeight+45*(i+1)});
+   });
+
+// text1.select('tspan:nth-of-type(2)').attr({transform: 'rotate(10 x y'});
+
+// block.attr({
+//   width: (text1.node.clientWidth + 50)
+// });
+
+
+
+
 
 
 /* Call all animation functions */
