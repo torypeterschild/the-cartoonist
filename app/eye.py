7bc1a8b204e98661f14740ebf6515eb4a8b50a48
Tory Peterschild <torypeterschild@Torys-MacBook-Pro.local>
2016-07-12 12:50:52 -0400

    added lash/lid generation

--- app/eye.py
+++ app/eye.py
@@ -1,6 +1,7 @@
 import svgwrite
 import path_utilities as pu
 import noise
+import math
 
 
 class Eye:
@@ -10,23 +11,54 @@ class Eye:
     self.cx = cx
     self.cy = cy
     self.outline = pu.create_circ_points(n, r, cx, cy)
+    self.outline_e = svgwrite.shapes.Ellipse(center=(cx*noise.rN(),cy*noise.rN()),
+      r=(r*noise.rN(0.7,1.1), r*noise.rN(0.7,1.3)), fill=noise.rC(), opacity=0.7, 
+      stroke=noise.rC(), stroke_width=noise.rI(1,3))
     self.pupil = svgwrite.shapes.Circle(center=(cx*noise.rN(),cy*noise.rN()), 
-      r=(noise.rN()*r/5), fill=noise.rC(), opacity=0.7, stroke=noise.rC(), stroke_width=noise.rI(1,3))
+      r=(noise.rN()*r/5), fill=noise.rC(), opacity=0.7, stroke=noise.rC(), 
+      stroke_width=noise.rI(1,3))
+
+  def make_lashes(self):
+    cx = self.outline_e['cx']
+    cy = self.outline_e['cy']
+    rx = self.outline_e['rx']
+    ry = self.outline_e['ry']
+    path = svgwrite.path.Path('M %d,%d' % (cx+rx,cy))
+    path.fill('grey')
+    lash_path = svgwrite.path.Path('M %d,%d' % (cx+rx,cy))
+    lash_path.fill(noise.rC(),opacity=0.4).stroke("grey",width="1")
+    s = (2 * math.pi)/self.n
+    for i in range(self.n):
+      a = s * i
+      xp = cx + rx * math.cos(a)
+      yp = cy + ry * math.sin(a)
+      path.push('L %d,%d' % (xp,yp))
+      if math.pi < a < 2*math.pi:
+        dx = xp
+        dy = yp - 10
+        lash_path.push('L %d,%d' % (xp,yp))
+        lash_path.push('L %d,%d' % (dx,dy))
+    path.push('L %d,%d' % (cx+rx,cy))
+    return lash_path
 
   def translate(self, tx, ty=None):
     if ty is not None:
       self.outline.translate(tx, ty)
+      self.outline_e.translate(tx, ty)
       self.pupil.translate(tx, ty)
     else:
-      self.outline.translate(tx)  
+      self.outline.translate(tx)
+      self.outline_e.translate(tx) 
       self.pupil.translate(tx)
 
   def scale(self, sx, sy=None):
     if ty is not None:
       self.outline.scale(sx, sy)
+      self.outline_e.scale(sx, sy)
       self.pupil.scale(sx, sy)
     else:
-      self.outline.scale(sx)  
+      self.outline.scale(sx)
+      self.outline_e.scale(sx) 
       self.pupil.scale(sx)
 
   def __str__(self):
--- app/path_utilities.py
+++ app/path_utilities.py
@@ -133,3 +133,17 @@ def create_fuzzy_head(n, r, cx, cy):
     # path.push("S %d,%d %d,%d " % (new_x*noise.rN(),new_y*noise.rN(),new_x,new_y))
   return path
 
+
+"""
+  TOAST PATH
+"""
+# def create_path():
+#   d = ('M', Q0_X, Q0_Y)
+#   path = self.paper.path(d=d, fill=rC(), opacity=0.3, stroke='orange', stroke_width='3')
+#   path.push("S%d,%d %d,%d" % (X_MAX,Y_MIN,Q1_X,Q1_Y))
+#   path.push("C%d,%d %d,%d %d,%d" % (X_MAX*rN(),Y_MAX*rN(),X_MAX*rN(),Y_MAX*rN(),Q2_X*rN(),Q2_Y*rN()))
+#   path.push("C%d,%d %d,%d %d,%d" % (X_MIN*rN(),Y_MAX*rN(),X_MIN*rN(),Y_MAX*rN(),Q3_X*rN(),Q3_Y*rN()))
+#   path.push("C%d,%d %d,%d %d,%d" % (X_MIN*rN(),Y_MIN*rN(),X_MIN*rN(),Y_MIN*rN(),Q0_X,Q0_Y))
+#   print("\nCOMMANDS\n")
+#   print(path.commands)
+#   return path
