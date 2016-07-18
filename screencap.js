897663d8e94c3faf66f95c56edc30602ddc03eb3
Tory Peterschild <torypeterschild@Torys-MacBook-Pro.local>
2016-07-18 11:07:00 -0400

    removing screencap.js

--- screencap.js
+++
@@ -1,8 +0,0 @@
-var page = require('webpage').create();
-page.open('http://localhost:5000/input', function(status) {
-  console.log("Status: " + status);
-  if(status === "success") {
-    page.render('example.png');
-  }
-  phantom.exit();
-});
