07b1e7654030cef96f6c7907824fdfc8f896e3fa
torypeterschild <torypeterschild@gmail.com>
2016-08-04 14:41:44 -0400

    adding screencap script; does not work for localhost

---
+++ app/screencap.py
@@ -0,0 +1,6 @@
+from selenium import webdriver, DepotManager
+
+driver = webdriver.PhantomJS()
+driver.set_window_size(1024, 768) # set the window size that you need 
+driver.get('https://github.com/torypeterschild/the-cartoonist')
+driver.save_screenshot('github.png')
