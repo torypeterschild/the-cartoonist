52b3a7ef07b53f5f1757a05c42320fdcf200a896
torypeterschild <torypeterschild@gmail.com>
2016-08-13 15:15:47 -0400

    added spinner on regenerate click

--- app/static/js/draw.js
+++ app/static/js/draw.js
@@ -1,10 +1,14 @@
 
 /* This saves SVG file (without caption) */
-$('#SVGsave').click(function(){
-    var a      = document.createElement('a');
-    a.href     = 'data:image/svg+xml;utf8,' + unescape($('#dog')[0].outerHTML);
-    a.download = 'cartoon.svg';
-    a.target   = '_blank';
-    document.body.appendChild(a); a.click(); document.body.removeChild(a);
-});
+// $('#SVGsave').click(function(){
+//     var a      = document.createElement('a');
+//     a.href     = 'data:image/svg+xml;utf8,' + unescape($('#dog')[0].outerHTML);
+//     a.download = 'cartoon.svg';
+//     a.target   = '_blank';
+//     document.body.appendChild(a); a.click(); document.body.removeChild(a);
+// });
 
+
+$('.regenerate').click(function(){
+    $(this).find('i').toggleClass('fa-random fa-spinner fa-spin');
+});
